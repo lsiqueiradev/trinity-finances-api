06e0842fc8ee97fb81deb04defe49b3638bafcaf
Lucas Siqueira <lucas@lsiqueira.dev>
2025-09-05 14:43:24 -0300

    refactor: remove unused AuthenticationException handling from exception rendering

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,15 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->prepend(\App\Http\Middleware\JwtMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-
-        $exceptions->render(function (AuthenticationException $e, Request $request) {
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'message' => trans('auth.unauthorized'),
-                ], 401);
-            }
-        });
-
         $exceptions->render(function (HttpException $e, Request $request) {
             return response()->json([
                 'message' => $e->getMessage(),
